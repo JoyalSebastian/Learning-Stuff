a0b71665bae1d8f01639c4269e0c1a9bf5853c34
JoyalSebastian <74226972+JoyalSebastian@users.noreply.github.com>
2021-09-29 09:47:09 +0530

    Added learningstring.py

---
+++ learningstring.py
@@ -0,0 +1,12 @@
+name = "Joyal"
+
+print(len(name)) #returns the number of characters
+print(name.find("o")) #finds a specific character in a string
+print(name.capitalize()) #captializes only the first letter of the string
+print(name.upper()) #converts everything in the string to uppercase
+print(name.lower()) #same as above but lowercase
+print(name.isdigit()) #will return True or False depending on whether the string is a digit
+print(name.isalpha()) #will return True or False depending on whether the is made up of alphabets. spaces dont count
+print(name.count("o")) #will count how  much of a specific character is present in a string
+print(name.replace("o","a")) #will replace a specific string with another.
+print(name*3) #prints the name 3 times.
